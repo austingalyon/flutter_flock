2c31415e6173a7beebe1c06921bfb9464651366a
john <john@flutter_flock>
2019-06-01 14:53:32 -0400

    fix login button color to match flutter logo

--- lib/screens/login.dart
+++ lib/screens/login.dart
@@ -47,7 +47,7 @@ class _LoginState extends State<Login> {
     final loginButton = Material(
       elevation: 5.0,
       borderRadius: BorderRadius.circular(30.0),
-      color: Color(0xff01A0C7),
+      color: Color(0xff01579b),
       child: MaterialButton(
         minWidth: MediaQuery.of(context).size.width,
         padding: EdgeInsets.fromLTRB(20.0, 15.0, 20.0, 15.0),
