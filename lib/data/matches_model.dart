bb9f65aee51482014685f57ff34d43f6d9976c3a
Austin Galyon <austin.galyon@maxxpotential.com>
2019-06-01 15:24:51 -0400

    Add name to Matches json

--- lib/data/matches_model.dart
+++ lib/data/matches_model.dart
@@ -3,11 +3,15 @@ import 'topics_model.dart';
 
 class MatchModel {
   final String url;
+  final String name;
   final String description;
   final List<TopicsModel> topics;
 
   MatchModel(
-      {@required this.url, @required this.description, @required this.topics});
+      {@required this.url,
+      @required this.name,
+      @required this.description,
+      @required this.topics});
 
   factory MatchModel.fromJson(Map<String, dynamic> json) {
     var topic = json['topics'] as List;
@@ -15,6 +19,9 @@ class MatchModel {
         topic.map((i) => TopicsModel.fromJson(i)).toList();
 
     return MatchModel(
-        url: json['url'], description: json['description'], topics: topicsList);
+        url: json['url'],
+        name: json['name'],
+        description: json['description'],
+        topics: topicsList);
   }
 }
--- lib/screens/matches.dart
+++ lib/screens/matches.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'contact.dart';
 import '../data/matches_model.dart';
 import 'dart:convert';
 
@@ -18,7 +19,7 @@ class _MatchesState extends State<Matches> {
 
   List<MatchModel> _LoadData() {
     var exampleJson =
-        '[{"url": "https://avatars1.githubusercontent.com/u/14101776?s=200&v=4", "description": "This is just a filler descripiton", "topics": [{"name": "ios", "isExpert": true}, {"name": "packages", "isExpert": true}, {"name": "plugins", "isExpert": false}]}]';
+        '[{"url": "https://avatars1.githubusercontent.com/u/14101776?s=200&v=4", "name": "Flutter Flock", "description": "This is just a filler descripiton", "topics": [{"name": "ios", "isExpert": true}, {"name": "packages", "isExpert": true}, {"name": "plugins", "isExpert": false}]}]';
 
     var jsonDecoded = json.decode(exampleJson);
     var decoded =
@@ -36,27 +37,34 @@ class _MatchesState extends State<Matches> {
       ),
       body: ListView.builder(
         itemBuilder: (BuildContext context, int index) {
-          return Card(
-              child: Row(
-            children: <Widget>[
-              Image.network(
-                matches[index].url,
-                width: 50,
-                height: 50,
-              ),
-              Column(
-                children: <Widget>[
-                  Text("${matches[index].description}"),
-                  Row(
-                    children: <Widget>[
-                      for (var topic in matches[index].topics)
-                        Text("${topic.name}")
-                    ],
-                  )
-                ],
-              )
-            ],
-          ));
+          return GestureDetector(
+            onTap: () => Navigator.push(
+                context, MaterialPageRoute(builder: (context) => Contact())),
+            child: Card(
+                child: Row(
+              children: <Widget>[
+                Image.network(
+                  matches[index].url,
+                  width: 50,
+                  height: 50,
+                ),
+                Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: <Widget>[
+                    Text("${matches[index].name}"),
+                    Text("${matches[index].description}"),
+                    Row(
+                      mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                      children: <Widget>[
+                        for (var topic in matches[index].topics)
+                          if (topic.isExpert) Text(" •${topic.name}")
+                      ],
+                    )
+                  ],
+                )
+              ],
+            )),
+          );
         },
         itemCount: matches.length,
       ),
